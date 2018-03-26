0a8d9c0949cf68d96e63d8fd569f81826e9f8fe3
TomFraser <tr.fraser@bigpond.com>
2018-03-26 11:01:14 +1000

    Fixed hanging while waiting for camera data

--- Software/lib/CameraWrapper/CameraWrapper.cpp
+++ Software/lib/CameraWrapper/CameraWrapper.cpp
@@ -17,20 +17,23 @@ void Camera::getCamData(){
     memset(tempCamData, -1, sizeof(tempCamData));
     /* Gets camera data and set its to a number of variables */
     if(millis() - lastCall >= 20){
-        while(cameraSerial.read() != 42);
-        for(int i = 0; i < CAM_TRANSFER_NUM; i++){
-            while(!cameraSerial.available());
-            tempCamData[i] = cameraSerial.read();
+        if(cameraSerial.read() == 42){
+            for(int i = 0; i < CAM_TRANSFER_NUM; i++){
+                while(!cameraSerial.available());
+                tempCamData[i] = cameraSerial.read();
+            }
+            cameraSerial.read();
+
+            rawAngle = tempCamData[0];
+            strength = tempCamData[1];
+            _aSize = tempCamData[2];
+            _aAngle = tempCamData[3];
+            _dSize = tempCamData[4];
+            _dAngle = tempCamData[5];
+
+            lastCall = millis();
+        }else{
+            /* Cam Serial Not Ready, Just loop instead of waiting */
         }
-        cameraSerial.read();
-
-        rawAngle = tempCamData[0];
-        strength = tempCamData[1];
-        _aSize = tempCamData[2];
-        _aAngle = tempCamData[3];
-        _dSize = tempCamData[4];
-        _dAngle = tempCamData[5];
-
-        lastCall = millis();
     }
 }
