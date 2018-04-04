8c282f4bf016c9beab64fc29c9e5a7dda88d23e7
TomFraser <tr.fraser@bigpond.com>
2018-04-04 16:02:06 +1000

    Silly Changes

--- Software/Main/src/Main.cpp
+++ Software/Main/src/Main.cpp
@@ -1,10 +1,6 @@
 #include <SPIWrapper.h>
 #include <MotorArray.h>
-<<<<<<< HEAD
 #include <Common.h>
-=======
-// #include <Common.h>
->>>>>>> f2b2f2b29304cf12d1d28c5e7105affe275cd526
 #include <Orbit.h>
 #include <IMU.h>
 #include <RotationWrapper.h>
