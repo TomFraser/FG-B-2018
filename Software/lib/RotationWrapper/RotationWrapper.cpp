af7b2a410c9ee33bdd6ffc85fdb9aec56d39d14a
TomFraser <tr.fraser@bigpond.com>
2018-04-10 11:09:14 +1000

    I hate line endings.

--- Software/lib/RotationWrapper/RotationWrapper.cpp
+++ Software/lib/RotationWrapper/RotationWrapper.cpp
@@ -4,20 +4,24 @@ RotationWrapper rotation = RotationWrapper();
 
 void RotationWrapper::calculateRotation(double rotation, double goal, double goalSize){
     /* Calculate the rotation from both camera and IMU */
-    gyration = IMUController.update(rotation, 0.00, 0.00);
+    compassHeading = IMUController.update(rotation, 0.00, 0.00);
 
-    // goalHeading = doubleMod(goal + 180, 360.0) - 180;
-    // goalHeading = goalController.update(goalHeading, 0.00, 0.00);
-    // goalHeading = goalHeading * IMU_MULTI;
-
-    // if(goal <= 0 || abs(goalHeading/IMU_MULTI) > GOAL_CUTOFF){
-        // if(signbit(compassHeading) && abs(compassHeading) > ROTATION_CUTOFF){
-        //     gyration = -ROTATION_CUTOFF;
-        // }else if(abs(compassHeading) > ROTATION_CUTOFF){
-        //     ROTATION_CUTOFF;
-        // }
+    // if(goal != 65506){
+    //     goalHeading = doubleMod(goal + 180, 360.0) - 180;
+    //     goalHeading = goalController.update(goalHeading, 0.00, 0.00);
+    //     goalHeading = goalHeading;
     // }else{
-        // gyration = signbit(goalHeading) && abs(goalHeading > ROTATION_CUTOFF) ? -ROTATION_CUTOFF : ROTATION_CUTOFF;
+    //     goalHeading = 65506;
+    // }
+    //
+    // if(goalHeading != 65506){
+    //     if(signbit(compassHeading) && abs(compassHeading) > ROTATION_CUTOFF){
+    //         gyration = -ROTATION_CUTOFF;
+    //     }else if(abs(compassHeading) > ROTATION_CUTOFF){
+    //         ROTATION_CUTOFF;
+    //     }
+    // }else{
+        gyration = compassHeading;
     // }
 }
 
